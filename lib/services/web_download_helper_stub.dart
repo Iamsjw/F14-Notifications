f5804c24a132d74f96e60c89a94a0588fb980a1c
Nova <131579958+Iamsjw@users.noreply.github.com>
2026-08-14 01:07:49 +0530

    Delete lib directory

--- lib/services/web_download_helper_stub.dart
+++
@@ -1,7 +0,0 @@
-void downloadWebCsv(String csvContent, String fileName) {
-  // No-op on mobile/desktop
-}
-
-void downloadWebText(String content, String fileName, String mimeType) {
-  // No-op on mobile/desktop
-}
